ef3787057cbb14cb2e1ba27ea3f6ca15ab46c054
fyodor <cn.fyodor@gmail.com>
2011-02-28 13:40:15 +0800

    add Ubuntu fonts

--- products/tattoo_custom.mk
+++ products/tattoo_custom.mk
@@ -158,3 +158,11 @@ PRODUCT_COPY_FILES += \
 	device/htc/tattoo/prebuilt/system/lib/librs_jni.so:system/lib/librs_jni.so \
 	device/htc/tattoo/prebuilt/system/lib/libt9.so:system/lib/libt9.so
 
+# Ubuntu fonts
+PRODUCT_COPY_FILES += \
+	device/htc/tattoo/prebuilt/system/fonts/Clockopia.ttf:system/fonts/Clockopia.ttf \
+	device/htc/tattoo/prebuilt/system/fonts/DroidSans.ttf:system/fonts/DroidSans.ttf \
+	device/htc/tattoo/prebuilt/system/fonts/DroidSansFallback.ttf:system/fonts/DroidSansFallback.ttf \
+	device/htc/tattoo/prebuilt/system/fonts/DroidSansMono.ttf:system/fonts/DroidSansMono.ttf \
+	device/htc/tattoo/prebuilt/system/fonts/DroidSans-Bold.ttf:system/fonts/DroidSans-Bold.ttf
+
